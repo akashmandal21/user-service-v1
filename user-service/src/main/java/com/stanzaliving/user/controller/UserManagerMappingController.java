3b78932217e7722a6f818630238b7fe011230b6a
Naveen Kumar <57009872+naveen-stanza@users.noreply.github.com>
2020-08-27 11:48:08 +0530

    Added message handling for Mobile and Email verification (#36)

--- user-service/src/main/java/com/stanzaliving/user/controller/UserManagerMappingController.java
+++ user-service/src/main/java/com/stanzaliving/user/controller/UserManagerMappingController.java
@@ -33,21 +33,21 @@ public class UserManagerMappingController {
 
 	@Autowired
 	private UserManagerMappingService userManagerMappingService;
-	
+
 	@PostMapping("create")
 	public ResponseDto<Void> createMapping(@RequestBody @Valid UserManagerMappingRequestDto userManagerMappingRequestDto) {
 		log.info(" Create userId and managerId mapping for : " + userManagerMappingRequestDto.getUserId() + " " + userManagerMappingRequestDto.getManagerId());
 
 		userManagerMappingService.createUserManagerMapping(userManagerMappingRequestDto);
-		
+
 		return ResponseDto.success("Mapping Created Successfully ");
 	}
-	
+
 	@GetMapping("{managerId}")
 	public ResponseDto<List<String>> getUserIdsByManagerId(@PathVariable(name = "managerId") @NotBlank(message = "Manager Id is Mandatory") String managerId) {
 		log.info(" Get mapped userIds by managerId " + managerId);
 
-		List<String> userIds = userManagerMappingService.getUserIdsMappedWithManagerId(managerId);		
+		List<String> userIds = userManagerMappingService.getUserIdsMappedWithManagerId(managerId);
 		return ResponseDto.success("Mapping Created Successfully", userIds);
 	}
 
@@ -55,7 +55,7 @@ public class UserManagerMappingController {
 	public ResponseDto<String> getManagerNameByUserID(@PathVariable(name = "userId") @NotBlank(message = "User Id is Mandatory") String userId) {
 		log.info(" Get manager name by " + userId);
 
-		String managerName = userManagerMappingService.findManagerNameForUser(userId);		
+		String managerName = userManagerMappingService.findManagerNameForUser(userId);
 		return ResponseDto.success("Manager Name Found!", managerName);
 	}
 
@@ -63,54 +63,52 @@ public class UserManagerMappingController {
 	public ResponseDto<UserProfileDto> getManagerProfieByUserID(@PathVariable(name = "userId") @NotBlank(message = "User Id is Mandatory") String userId) {
 		log.info(" Get manager profile by " + userId);
 
-		UserProfileDto managerProfile = userManagerMappingService.getManagerProfileForUser(userId);	
-		
-		if(managerProfile == null)
+		UserProfileDto managerProfile = userManagerMappingService.getManagerProfileForUser(userId);
+
+		if (managerProfile == null)
 			return ResponseDto.failure("Manager Profile Not Found");
-		
+
 		return ResponseDto.success("Manager Profile Found!", managerProfile);
 	}
-	
+
 	@PostMapping("/managerprofiles")
 	public ResponseDto<Map<String, UserProfileDto>> getManagerProfileByUserID(@RequestBody UserManagerProfileRequestDto profileRequestDto) {
 		log.info(" Get manager profiles by " + profileRequestDto.getUserUuids());
 
-		Map<String, UserProfileDto> userManagerMap = userManagerMappingService.getManagerProfileForUserIn(profileRequestDto.getUserUuids());	
-		
-		if(userManagerMap == null)
+		Map<String, UserProfileDto> userManagerMap = userManagerMappingService.getManagerProfileForUserIn(profileRequestDto.getUserUuids());
+
+		if (userManagerMap == null)
 			return ResponseDto.failure("Manager Profiles Not Found");
-		
+
 		return ResponseDto.success("Manager Profile Found!", userManagerMap);
 	}
-	
+
 	@GetMapping("/userprofiles/{managerId}")
 	public ResponseDto<List<UserProfileDto>> getUserProfilesByManagerID(@PathVariable("managerId") String managerId) {
 		log.info(" Get user profiles by " + managerId);
 
-		List<UserProfileDto> userManagerList = userManagerMappingService.getPeopleReportingToManager(managerId);	
-		
-		if(CollectionUtils.isEmpty(userManagerList))
+		List<UserProfileDto> userManagerList = userManagerMappingService.getPeopleReportingToManager(managerId);
+
+		if (CollectionUtils.isEmpty(userManagerList))
 			return ResponseDto.failure("User Profiles Not Found");
-		
+
 		return ResponseDto.success("User Profile Found!", userManagerList);
 	}
-	
+
 	@GetMapping("/managerprofile/{userId}/{managertype}")
 	public ResponseDto<UserProfileDto> getManagerProfieByUserID(@PathVariable(name = "userId") @NotBlank(message = "User Id is Mandatory") String userId,
 			@PathVariable(name = "managertype") @NotBlank(message = "Manager Type cannot be blank") UserManagerMappingType userManagerMappingType) {
-		
-		UserProfileDto managerProfile = userManagerMappingService.getUserManagerMappingHierarchy(userId, userManagerMappingType);		
-		
-		if(managerProfile == null)
+
+		UserProfileDto managerProfile = userManagerMappingService.getUserManagerMappingHierarchy(userId, userManagerMappingType);
+
+		if (managerProfile == null)
 			return ResponseDto.failure("Manager Mapping Not Found");
-		
+
 		return ResponseDto.success("Manager Mapping Found!", managerProfile);
 	}
 
 	@PostMapping("remove/{uuid}")
-	public ResponseDto<Void> removeManager(
-			@PathVariable("uuid") String mappingUuid
-	){
+	public ResponseDto<Void> removeManager(@RequestParam("uuid") String mappingUuid) {
 		userManagerMappingService.deleteManagerMapping(mappingUuid);
 		return ResponseDto.success("Manager removed successfully.");
 	}
