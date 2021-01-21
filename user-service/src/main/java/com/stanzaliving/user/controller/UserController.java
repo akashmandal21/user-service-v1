8bafcb7515fb6b0bcec33b66076e6bce5c134ba0
Mohit Gupta <mohitgupta.retainer@stanzaliving.com>
2021-01-21 22:16:23 +0530

    function move in internal controller

--- user-service/src/main/java/com/stanzaliving/user/controller/UserController.java
+++ user-service/src/main/java/com/stanzaliving/user/controller/UserController.java
@@ -166,22 +166,5 @@ public class UserController {
 
 		return response ? ResponseDto.success("User type and department modified successfully") : ResponseDto.failure("User type and department not modified");
 	}
-	
-	
-
-	@PostMapping("/internal/search/get/userUuid")
-	public ResponseDto<Map<String,UserProfileDto>> getUserProfileDto(@RequestBody Set<String> mobileNos
-			) {
-
-		log.info("Request received for getting user details");
-		
-		Map<String,UserProfileDto> userlist = userService.getUserProfileDto(mobileNos);
-
-		log.info("Successfully fetched user details.");
-		
-		return ResponseDto.success("Found user Details with manager and role details.", userlist);
-	}
-	
-	
 
 }
