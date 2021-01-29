829bcb3a86937909e5ba2a92aa155cec3a56d7e4
kapil-stanza <kapil.dev@stanzaliving.com>
2021-01-29 13:33:08 +0530

    Added constants file and getNotificationTemplateId method

---
+++ user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
@@ -0,0 +1,16 @@
+package com.stanzaliving.user.constants;
+
+public class NotificationKeys {
+
+	public static final String MOBILE_VERIFICATION_OTP_MSG="";
+	public static final String EMAIL_VERIFICATION_OTP_MSG="";
+	public static final String USER_VERIFICATION_OTP_MSG="";
+	public static final String STUDENT_OTP_MSG="";
+	public static final String PARENT_OTP_MSG="";
+	public static final String LEGAL_OTP_MSG="";
+	public static final String HR_OTP_MSG="";
+	public static final String TECH_OTP_MSG="";
+	public static final String FINANCE_OTP_MSG="";
+	public static final String PROCUREMENT_OTP_MSG="";
+	public static final String DEFAULT_OTP_MSG="";
+}
