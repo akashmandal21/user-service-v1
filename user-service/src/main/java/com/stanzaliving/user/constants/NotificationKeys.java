dfe167c1b58354315723a808da3bb7e4cc698629
Naveen Kumar <57009872+naveen-stanza@users.noreply.github.com>
2021-01-30 19:20:39 +0530

    Update NotificationKeys.java

--- user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
+++ user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
@@ -3,7 +3,7 @@ package com.stanzaliving.user.constants;
 public class NotificationKeys {
 
 	public static final String MOBILE_VERIFICATION_OTP_MSG="1707161192372443359";
-	public static final String EMAIL_VERIFICATION_OTP_MSG="1707161192360641779";
+	public static final String EMAIL_VERIFICATION_OTP_MSG="1707161200356744733";
 	public static final String USER_VERIFICATION_OTP_MSG="1707161192375967152";
 	public static final String STUDENT_OTP_MSG="1707161192360641779";
 	public static final String PARENT_OTP_MSG="1707161192360641779";
