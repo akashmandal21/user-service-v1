31005c9d713564f7baf21747cb645ef7b63041b9
navratan-ch <73222616+navratan-ch@users.noreply.github.com>
2021-09-20 19:34:53 +0530

    Revert "Update InternalOtpController.java"
    This reverts commit bc8b30b88a32cce3809dbdc77389b4328b279ea0.

--- user-service/src/main/java/com/stanzaliving/user/controller/internal/InternalOtpController.java
+++ user-service/src/main/java/com/stanzaliving/user/controller/internal/InternalOtpController.java
@@ -38,28 +38,22 @@ public class InternalOtpController {
 	@PostMapping("mobile/validate")
 	public ResponseDto<Void> validateMobileOtp(@RequestBody @Valid MobileOtpValidateRequestDto mobileOtpValidateRequestDto) {
 
-		
-		log.info("=====> Received request to validate OTP: {}", mobileOtpValidateRequestDto);
+		log.info("Received request to validate OTP: {}", mobileOtpValidateRequestDto);
 
-		return ResponseDto.success("OTP Succefully Validated");
+		try {
 
-		
-//		log.info("Received request to validate OTP: {}", mobileOtpValidateRequestDto);
-//
-//		try {
-//
-//			otpService.validateMobileOtp(
-//					mobileOtpValidateRequestDto.getMobile(),
-//					mobileOtpValidateRequestDto.getIsoCode(),
-//					mobileOtpValidateRequestDto.getOtp(),
-//					mobileOtpValidateRequestDto.getOtpType());
-//
-//			return ResponseDto.success("OTP Succefully Validated");
-//
-//		} catch (AuthException e) {
-//			log.error(e.getMessage());
-//			return ResponseDto.failure(e.getMessage());
-//		}
+			otpService.validateMobileOtp(
+					mobileOtpValidateRequestDto.getMobile(),
+					mobileOtpValidateRequestDto.getIsoCode(),
+					mobileOtpValidateRequestDto.getOtp(),
+					mobileOtpValidateRequestDto.getOtpType());
+
+			return ResponseDto.success("OTP Succefully Validated");
+
+		} catch (AuthException e) {
+			log.error(e.getMessage());
+			return ResponseDto.failure(e.getMessage());
+		}
 	}
 
 	@PostMapping("mobile/resend")
