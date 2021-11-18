1f011ed67b76ccbf252434f915ffcff288208668
Prateek Ladha <>
2021-11-18 19:28:55 +0530

    Alfred otp sms template id changed

--- user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
+++ user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
@@ -2,15 +2,19 @@ package com.stanzaliving.user.constants;
 
 public class NotificationKeys {
 
+	//{#var#} is OTP to verify your phone number with Stanza Living
 	public static final String MOBILE_VERIFICATION_OTP_MSG="1707161225594385472";
+	//{#var#} is OTP to verify your email with Stanza Living
 	public static final String EMAIL_VERIFICATION_OTP_MSG="1707161200356744733";
+	//{#var#} is your OTP to authenticate with Stanza Living
 	public static final String USER_VERIFICATION_OTP_MSG="1707161192380239187";
-	public static final String STUDENT_OTP_MSG="1707161192360641779";
-	public static final String PARENT_OTP_MSG="1707161192360641779";
-	public static final String LEGAL_OTP_MSG="1707161192360641779";
-	public static final String HR_OTP_MSG="1707161192360641779";
-	public static final String TECH_OTP_MSG="1707161192360641779";
-	public static final String FINANCE_OTP_MSG="1707161192360641779";
-	public static final String PROCUREMENT_OTP_MSG="1707161192360641779";
-	public static final String DEFAULT_OTP_MSG="1707161192360641779";
+	//{#var#} is OTP to verify your phone number for accessing StanzaLiving.
+	public static final String STUDENT_OTP_MSG="1707163695477631286";
+	public static final String PARENT_OTP_MSG="1707163695477631286";
+	public static final String LEGAL_OTP_MSG="1707163695477631286";
+	public static final String HR_OTP_MSG="1707163695477631286";
+	public static final String TECH_OTP_MSG="1707163695477631286";
+	public static final String FINANCE_OTP_MSG="1707163695477631286";
+	public static final String PROCUREMENT_OTP_MSG="1707163695477631286";
+	public static final String DEFAULT_OTP_MSG="1707163695477631286";
 }
