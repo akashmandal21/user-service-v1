71d1bb85805c5619117a4c1b2f09abacc379769e
Prateek Ladha <>
2021-11-24 23:29:35 +0530

    SMS templateId updated for message - {#var#} is OTP to verify your phone number with Stanza Living and {#var#} is your OTP to authenticate with Stanza Living

--- user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
+++ user-service/src/main/java/com/stanzaliving/user/constants/NotificationKeys.java
@@ -3,11 +3,11 @@ package com.stanzaliving.user.constants;
 public class NotificationKeys {
 
 	//{#var#} is OTP to verify your phone number with Stanza Living
-	public static final String MOBILE_VERIFICATION_OTP_MSG="1707161225594385472";
+	public static final String MOBILE_VERIFICATION_OTP_MSG="1707163756428389949";
 	//{#var#} is OTP to verify your email with Stanza Living
 	public static final String EMAIL_VERIFICATION_OTP_MSG="1707161200356744733";
 	//{#var#} is your OTP to authenticate with Stanza Living
-	public static final String USER_VERIFICATION_OTP_MSG="1707161192380239187";
+	public static final String USER_VERIFICATION_OTP_MSG="1707163756436461040";
 	//{#var#} is OTP to verify your phone number for accessing StanzaLiving.
 	public static final String STUDENT_OTP_MSG="1707163695477631286";
 	public static final String PARENT_OTP_MSG="1707163695477631286";
