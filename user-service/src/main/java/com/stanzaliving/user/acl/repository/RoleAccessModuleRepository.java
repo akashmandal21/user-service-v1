770b27046c26dd561710ee5ea816d90635c11478
Harpreet Singh <harpreet.singh@unthinkable.co>
2022-03-01 10:05:13 +0530

    User Access Module API

---
+++ user-service/src/main/java/com/stanzaliving/user/acl/repository/RoleAccessModuleRepository.java
@@ -0,0 +1,19 @@
+package com.stanzaliving.user.acl.repository;
+
+import com.stanzaliving.core.base.enums.AccessModule;
+import com.stanzaliving.core.sqljpa.repository.AbstractJpaRepository;
+import com.stanzaliving.user.acl.entity.RoleAccessModuleMappingEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleAccessModuleRepository extends AbstractJpaRepository<RoleAccessModuleMappingEntity, Long> {
+
+    @Query(value = "SELECT DISTINCT r.accessModule " +
+        "FROM com.stanzaliving.user.acl.entity.RoleAccessModuleMappingEntity r " +
+        "WHERE r.roleUuid IN :roleUuids " +
+        "AND r.status = :status")
+    List<AccessModule> findAccessModuleByRoleUuidInAndStatus(List<String> roleUuids, boolean status);
+}
