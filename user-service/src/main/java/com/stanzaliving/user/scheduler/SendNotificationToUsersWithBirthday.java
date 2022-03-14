2866c8a7282666307acd85120042fb168d248726
manirajAgarwal <90244257+manirajAgarwal@users.noreply.github.com>
2022-03-14 19:18:23 +0530

    Scheduler added for sending birthday notifications.

--- user-service/src/main/java/com/stanzaliving/user/scheduler/SendNotificationToUsersWithBirthday.java
+++ user-service/src/main/java/com/stanzaliving/user/scheduler/SendNotificationToUsersWithBirthday.java
@@ -1,12 +1,19 @@
 package com.stanzaliving.user.scheduler;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.stanzaliving.core.base.common.dto.ResponseDto;
+import com.stanzaliving.core.base.utils.DateUtil;
+import com.stanzaliving.core.notificationv2.api.NotificationClientApi;
+import com.stanzaliving.notification.dto.NotificationDTO;
+import com.stanzaliving.user.service.PayloadService;
 import com.stanzaliving.user.service.UserService;
 
 import lombok.extern.log4j.Log4j2;
@@ -21,8 +28,14 @@ public class SendNotificationToUsersWithBirthday {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private PayloadService payloadService;
 	
-	 @Scheduled(cron = "0 23 13 * * *")
+    @Autowired
+    private NotificationClientApi notificationClientApi;
+	
+	
+	 @Scheduled(cron = "0 30 19 * * *")
 	   public void cronJobSch() {
 
 		 log.info("Send notification to users who have birthday today:: Starting send notifications to users having birthday today Job");
@@ -35,6 +48,16 @@ public class SendNotificationToUsersWithBirthday {
 					
 					log.info("List is: {}", userIdList.toString());
 					
+					NotificationDTO payload=payloadService.getNotificationRegistryPayload(userIdList,DateUtil.convertToDate(LocalDateTime.now()));
+					log.info("Payload for Notification:{}",payload.getTitle());
+					
+					ResponseDto<NotificationDTO> saveGenericNotification = notificationClientApi.saveGenericNotification(payload);
+			        
+					if (saveGenericNotification.isStatus()) {
+						NotificationDTO dto = saveGenericNotification.getData();
+						log.info("Notification dto is: {}", dto);
+					}
+				
 				}
 				
 				else {
