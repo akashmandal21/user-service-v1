c1c8f3c3d05380493213b0dce25c206d147b1886
manirajAgarwal <90244257+manirajAgarwal@users.noreply.github.com>
2022-03-16 11:34:43 +0530

    cron changes for test

--- user-service/src/main/java/com/stanzaliving/user/scheduler/SendNotificationToUsersWithBirthday.java
+++ user-service/src/main/java/com/stanzaliving/user/scheduler/SendNotificationToUsersWithBirthday.java
@@ -35,7 +35,7 @@ public class SendNotificationToUsersWithBirthday {
     private NotificationClientApi notificationClientApi;
 
 	  // @Scheduled(cron = "0 0 0 * * *")
-       @Scheduled(cron = "0 19 11 * * *")
+       @Scheduled(cron = "0 38 11 * * *")
 	   public void cronJobSch() {
 
 		 log.info("Send notification to users who have birthday today:: Starting send notifications to users having birthday today Job");
