60d2b681c5c04d7d083fa353727c78287c2d951f
Harpreet Singh <harpreet.singh@unthinkable.co>
2022-04-01 11:01:26 +0530

    Removed commented code

--- user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
+++ user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
@@ -826,31 +826,6 @@ public class AclUserServiceImpl implements AclUserService {
 		return userAccessModuleDtoList;
 	}
 
-//	@Override
-//	public List<CityMetadataDto> getCitiesByUserAcessAndDepartment(String userUuid, Department department) {
-//		log.info("Get cities for user : {} and department : {}", userUuid, department);
-//		UserDepartmentLevelEntity userDepartmentLevelEntitiesByCountry = userDepartmentLevelRepository
-//			.findByUserUuidAndDepartmentAndAccessLevelAndStatus(userUuid, department, AccessLevel.COUNTRY, true);
-//		if (Objects.nonNull(userDepartmentLevelEntitiesByCountry)) {
-//			return transformationCache.getAllCities();
-//		} else {
-//			UserDepartmentLevelEntity userDepartmentLevelEntitiesByCity = userDepartmentLevelRepository
-//				.findByUserUuidAndDepartmentAndAccessLevelAndStatus(userUuid, department, AccessLevel.CITY, true);
-//			if (Objects.nonNull(userDepartmentLevelEntitiesByCity)) {
-//				List<String> cityUuids = Arrays.asList(userDepartmentLevelEntitiesByCity.getCsvAccessLevelEntityUuid().split(","));
-//				List<CityMetadataDto> cityMetadataDtos = new ArrayList<>();
-//				for (String cityUuid : cityUuids) {
-//					if (CollectionUtils.isNotEmpty(cityUuids)) {
-//						cityMetadataDtos.add(transformationCache.getCityByUuid(cityUuid));
-//					}
-//				}
-//				return cityMetadataDtos;
-//			} else {
-//				return null;
-//			}
-//		}
-//	}
-
 	@Override
 	public List<CityMetadataDto> getCitiesByUserAcessAndDepartment(String userUuid, Department department) {
 		log.info("Get cities for user : {} and department : {}", userUuid, department);
