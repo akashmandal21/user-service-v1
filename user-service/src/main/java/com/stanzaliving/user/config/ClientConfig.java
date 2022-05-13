e3bb31ab4857aebd9f4a437ce0870c8bf57331cc
yuktigupta2510 <yukti.gupta@stanzaliving.com>
2022-05-13 14:36:01 +0530

    added bean

--- user-service/src/main/java/com/stanzaliving/user/config/ClientConfig.java
+++ user-service/src/main/java/com/stanzaliving/user/config/ClientConfig.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.user.config;
 
+import com.stanzaliving.core.leadservice.client.api.LeadserviceClientApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,9 @@ public class ClientConfig {
     @Value("${service.transformationmaster.url}")
     private String transformationUrl;
 
+    @Value("${service.lead.url}")
+    private String leadUrl;
+
     @Bean
     public InternalDataControllerApi internalDataControllerApi() {
         return new InternalDataControllerApi(new StanzaRestClient(transformationUrl));
@@ -22,5 +26,10 @@ public class ClientConfig {
     @Bean
     public TransformationCache transformationCache(InternalDataControllerApi internalDataControllerApi) {
         return new TransformationCache(internalDataControllerApi);
+    }
+
+    @Bean
+    public LeadserviceClientApi LeadserviceClientApi() {
+        return new LeadserviceClientApi(new StanzaRestClient(leadUrl));
     }
 }
