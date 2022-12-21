23f8439a4d59ca82de18803767e30db08999173e
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2022-12-21 15:21:02 +0530

    added changes

--- user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
+++ user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
@@ -146,7 +146,7 @@ public class AclUserServiceImpl implements AclUserService {
 	public void addRole(AddUserDeptLevelRoleRequestDto addUserDeptLevelRoleDto) {
 
 		UserEntity user=userDbService.findByUuidNotMigrated(addUserDeptLevelRoleDto.getUserUuid(),false);
-		if(Objects.nonNull(user)){
+		if(Objects.isNull(user)){
 			throw new StanzaException("User might be migrated/created in acl2.0,please use new user management to assing permissions for this user.");
 		}
 
