b89e39b05f5b2b4a19a7296d51a3b046777c570e
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2023-02-17 16:50:39 +0530

    added changes

--- user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
+++ user-service/src/main/java/com/stanzaliving/user/acl/service/impl/AclUserServiceImpl.java
@@ -214,24 +214,6 @@ public class AclUserServiceImpl implements AclUserService {
 			roleDtoList.addAll(RoleAdapter.getDtoList(roleEntities));
 		}
 		return roleDtoList;
-//		List<RoleDto> roleDtos=userV2FeignService.getRolesFromUserUuid(userUuid);
-//
-//		List<RoleDto> roleDtoList = new ArrayList<>();
-//		List<UserDepartmentLevelRoleEntity> userDepartmentLevelRoleEntityList;
-//		List<UserDepartmentLevelEntity> userDepartmentLevelEntityList = userDepartmentLevelDbService.findByUserUuidAndStatus(userUuid, true);
-//		List<String> roleUuids;
-//
-//		for (UserDepartmentLevelEntity userDepartmentLevelEntity : userDepartmentLevelEntityList) {
-//			userDepartmentLevelRoleEntityList = userDepartmentLevelRoleDbService.findByUserDepartmentLevelUuidAndStatus(userDepartmentLevelEntity.getUuid(), true);
-//			roleUuids = userDepartmentLevelRoleEntityList.parallelStream().map(UserDepartmentLevelRoleEntity::getRoleUuid).collect(Collectors.toList());
-//			List<RoleEntity> roleEntities = roleDbService.findByUuidInAndStatus(roleUuids, true);
-//			roleDtoList.addAll(RoleAdapter.getDtoList(roleEntities));
-//		}
-//
-//		if(Objects.nonNull(roleDtos) && roleDtos.size()>0){
-//			roleDtoList.addAll(roleDtos);
-//		}
-//		return roleDtoList;
 	}
 
 	@Override
