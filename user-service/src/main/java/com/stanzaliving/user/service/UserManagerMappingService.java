3b69885529034a9ad6071d5e31d3d15e653044b2
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2023-03-16 14:22:14 +0530

    added changes

--- user-service/src/main/java/com/stanzaliving/user/service/UserManagerMappingService.java
+++ user-service/src/main/java/com/stanzaliving/user/service/UserManagerMappingService.java
@@ -6,6 +6,7 @@ package com.stanzaliving.user.service;
 import java.util.List;
 import java.util.Map;
 
+import com.stanzaliving.core.base.enums.Department;
 import com.stanzaliving.core.user.dto.UserProfileDto;
 import com.stanzaliving.core.user.enums.UserManagerMappingType;
 import com.stanzaliving.core.user.request.dto.UserManagerMappingRequestDto;
@@ -29,6 +30,8 @@ public interface UserManagerMappingService {
 	Map<String, UserProfileDto> getManagerProfileForUserIn(List<String> userIds);
 	
 	List<UserProfileDto> getPeopleReportingToManager( String managerId );
+
+	List<UserProfileDto> getPeopleReportingToManagerForZonalHead(String managerId , Department department);
 
 	void deleteManagerMapping(String userUuid);
 }
