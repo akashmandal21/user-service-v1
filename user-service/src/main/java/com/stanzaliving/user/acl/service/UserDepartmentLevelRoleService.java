22a625f758be618a5404a4e2820c77d0d959684d
jayapavan42 <jaya.kadimetla@stanzaliving.com>
2023-04-25 19:20:08 +0530

    added changes

--- user-service/src/main/java/com/stanzaliving/user/acl/service/UserDepartmentLevelRoleService.java
+++ user-service/src/main/java/com/stanzaliving/user/acl/service/UserDepartmentLevelRoleService.java
@@ -1,5 +1,6 @@
 package com.stanzaliving.user.acl.service;
 
+import com.stanzaliving.user.acl.entity.UserDepartmentLevelEntity;
 import com.stanzaliving.user.acl.entity.UserDepartmentLevelRoleEntity;
 
 import java.util.List;
@@ -8,7 +9,7 @@ public interface UserDepartmentLevelRoleService {
 
     List<UserDepartmentLevelRoleEntity> addRoles(String userDepartmentLevelUuid, List<String> rolesUuid);
 
-    void revokeRoles(String uuid, List<String> rolesUuid);
+    void revokeRoles(UserDepartmentLevelEntity userDepartmentLevelEntity, List<String> rolesUuid);
 
 	List<UserDepartmentLevelRoleEntity> findByRoleUuid(String roleUuid);
     
