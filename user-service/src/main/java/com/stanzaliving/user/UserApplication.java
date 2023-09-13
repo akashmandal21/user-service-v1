6131a851f51435bfeb3896279ad87f9d62eab442
Ashutosh.Gupta <ashutosh.gupta@stanzaliving.com>
2023-09-13 17:27:32 +0530

    changes to run service on local

--- user-service/src/main/java/com/stanzaliving/user/UserApplication.java
+++ user-service/src/main/java/com/stanzaliving/user/UserApplication.java
@@ -11,12 +11,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableScheduling
 @EnableFeignClients
 @EnableAsync
 @ComponentScan(basePackages = { "com.stanzaliving" })
+@SpringBootApplication(exclude = {
+		org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextResourceLoaderAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.mail.MailSenderAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.cache.ElastiCacheAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.messaging.MessagingAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.jdbc.AmazonRdsDatabaseAutoConfiguration.class,
+		org.springframework.cloud.aws.autoconfigure.metrics.CloudWatchExportAutoConfiguration.class
+})
 public class UserApplication {
 
 	public static void main(String[] args) {
